7570febeecf4a4d1567a2874014c59c7c4044ae4
Brayan Hernandez <brayanhernandezfurnieles@gmail.com>
2025-01-27 19:46:24 -0500

    Temporarily disabling spring security

--- src/main/java/com/lexbot/security/SecurityConfig.java
+++ src/main/java/com/lexbot/security/SecurityConfig.java
@@ -1,4 +1,24 @@
 package com.lexbot.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+
+    // TODO: This is temporary, it will be replaced by JWT
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .formLogin(login -> login.disable())
+            .authorizeHttpRequests(request -> request.anyRequest().permitAll());
+
+        return http.build();
+    }
+
 }
